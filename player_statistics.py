a37d172b4e5381ef055598478861f828ed96d02d
Jackson Annese <jackson.annese23@gmail.com>
2026-02-03 14:56:01 -0500

    Framework

---
+++ player_statistics.py
@@ -0,0 +1,29 @@
+# ====================
+# * Player Functions *
+# ====================
+
+def generate_season_stats():
+    pass
+
+def generate_weighted_stats():
+    pass
+
+# ===================
+# * Query Functions *
+# ===================
+
+def get_players():
+    pass
+
+def get_player_boxscores():
+    pass
+
+# ======================
+# * Workflow Functions *
+# ======================
+
+def generate_team_stats():
+    pass
+
+def generate_league_stats():
+    pass
